1ffd204b4ba69b12eafe74808a66294368bb8189
darlok77 <g.dalleau77@hotmail.com>
2024-09-28 00:19:13 +0200

    fix: add promise.all && fix index displaying loop

--- src/routes/_authentication/index.tsx
+++ src/routes/_authentication/index.tsx
@@ -21,8 +21,7 @@ import {
   GetMemeCommentsResponse,
   getMemes,
   GetMemesResponse,
-  getUserById,
-  GetUserByIdResponse,
+  getUserById
 } from "../../api";
 import { useAuthToken } from "../../contexts/authentication";
 import { Loader } from "../../components/loader";
@@ -37,32 +36,36 @@ export const MemeFeedPage: React.FC = () => {
     queryFn: async () => {
       const memes: GetMemesResponse["results"] = [];
       const firstPage = await getMemes(token, 1);
-      memes.push(...firstPage.results);
-      const remainingPages =
-        Math.ceil(firstPage.total / firstPage.pageSize) - 1;
-      for (let i = 0; i < remainingPages; i++) {
-        const page = await getMemes(token, i + 2);
+      const remainingPages = Math.round(firstPage.total / firstPage.pageSize);
+      for (let i = 1; i <= remainingPages; i++) {
+        const page = await getMemes(token, i);
         memes.push(...page.results);
       }
       const memesWithAuthorAndComments = [];
-      for (let meme of memes) {
-        const author = await getUserById(token, meme.authorId);
+      const memeAuthorsPromises = memes.map(meme => getUserById(token, meme.authorId));
+      const memeAuthors = await Promise.all(memeAuthorsPromises);
+
+      for (let i = 0; i < memes.length; i++) {
+        const meme = memes[i];
+        const author = memeAuthors[i];
+
         const comments: GetMemeCommentsResponse["results"] = [];
         const firstPage = await getMemeComments(token, meme.id, 1);
         comments.push(...firstPage.results);
-        const remainingCommentPages =
-          Math.ceil(firstPage.total / firstPage.pageSize) - 1;
-        for (let i = 0; i < remainingCommentPages; i++) {
-          const page = await getMemeComments(token, meme.id, i + 2);
+        const remainingCommentPages = Math.round(firstPage.total / firstPage.pageSize);
+        for (let i = 1; i <= remainingCommentPages; i++) {
+          const page = await getMemeComments(token, meme.id, i);
           comments.push(...page.results);
         }
-        const commentsWithAuthor: (GetMemeCommentsResponse["results"][0] & {
-          author: GetUserByIdResponse;
-        })[] = [];
-        for (let comment of comments) {
-          const author = await getUserById(token, comment.authorId);
-          commentsWithAuthor.push({ ...comment, author });
-        }
+
+        const commentAuthorsPromises = comments.map(comment => getUserById(token, comment.authorId));
+        const commentAuthors = await Promise.all(commentAuthorsPromises);
+
+        const commentsWithAuthor = comments.map((comment, index) => ({
+          ...comment,
+          author: commentAuthors[index],
+        }));
+
         memesWithAuthorAndComments.push({
           ...meme,
           author,
